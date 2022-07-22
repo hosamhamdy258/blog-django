d932bf797bf6ce8137c05fc390ad8e5e53e9926c
thesayedhesham <thesayedhesham@gmail.com>
2022-07-22 21:46:39 +0200

    aa

--- categories/views.py
+++ categories/views.py
@@ -1,19 +1,25 @@
+from cmath import log
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import is_valid_path
 from .models import Category, Post, Comment
 from .forms import NewPostForm, NewCommentForm
 from django.contrib.auth.decorators import login_required
-
+from taggit.models import Tag
 
 # Create your views here.
 
 def home(req):
     categories = Category.objects.all()
     posts = Post.objects.all().order_by('-created_dt')
+    # common_tags = []
+    # for post in posts:
+    #     common_tags += post.tags.names()
+    #     common_tags = list(set(common_tags))
 
+    common_tags = Post.tags.most_common()[:4]
     # user = User.objects.first()
-    return render(req, 'categories/home.html', {'categories': categories, "posts": posts})
+    return render(req, 'categories/home.html', {'categories': categories, "posts": posts, "common_tags": common_tags,})
 
 
 def new_category(req):
@@ -32,19 +38,38 @@ def category_posts(req, category_id):
     return render(req, 'categories/posts.html', {'category': category})
 
 
+# def detail_view(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#     return render(request, 'categories/detailed.html', {'post': post})
+# def tagged(request, slug):
+#     tag = get_object_or_404(Tag, slug=slug)
+#     posts = Post.objects.filter(tags=tag)
+#     context = {
+#         'tag':tag,
+#         'posts':posts,
+#     }
+#     return render(request, 'home.html', context)
+
 @login_required
 def new_post(req, category_id):
     category = get_object_or_404(Category, pk=category_id)
     # user = User.objects.first()
+    common_tags = Post.tags.most_common()[:4]
     if req.method == "POST":
         form = NewPostForm(req.POST)
+        
+
         if form.is_valid():
             # title and content of post is auto graped and saved by postform
             post = form.save(commit=False)
+            # post.slug = slugify(post.title)
             # here added extra data to be saved with post
             post.category = category
             post.created_by = req.user
+            post.tags = tags
             post.save()
+            form.save_m2m()
+            print(req.POST)
             return redirect('category_posts', category_id=category.pk)
     else:
         form = NewPostForm()
@@ -68,3 +93,4 @@ def post(req, category_id, post_id):
         form = NewCommentForm()
 
     return render(req, 'categories/post.html', {'post': post, 'form': form})
+
