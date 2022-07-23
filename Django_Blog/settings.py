dbae822b4cdc54ec02d34d234c524eb39d150a64
thesayedhesham <thesayedhesham@gmail.com>
2022-07-23 19:54:35 +0200

    edit3

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'accounts',
     'categories',
-    "taggit"
 ]
 
 MIDDLEWARE = [
--- categories/views.py
+++ categories/views.py
@@ -23,7 +23,7 @@ def homeTags(req, tag_slug):
     posts = Post.objects.filter(tags__slug=tag_slug)
     print(posts)
     # user = User.objects.first()
-    return render(req, 'categories/home.html', {'categories': categories, "posts": posts, "common_tags": common_tags,})
+    return render(req, 'categories/home.html', {'categories': categories, "posts": posts,})
 
 
 def new_category(req):
